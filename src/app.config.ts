b325fef16f40d1fda4c83629408460b1e14cde4b
KimJin <joji6666@github.com>
2023-08-25 14:47:07 +0900

    init

---
+++ src/app.config.ts
@@ -0,0 +1,58 @@
+import config from "@colyseus/tools";
+import { monitor } from "@colyseus/monitor";
+import { playground } from "@colyseus/playground";
+
+/**
+ * Import your Room files
+ */
+import { MyRoom } from "./rooms/MyRoom";
+
+export default config({
+  initializeGameServer: (gameServer) => {
+    /**
+     * Define your room handlers:
+     */
+    gameServer.define("my_room", MyRoom);
+  },
+
+  initializeExpress: (app) => {
+    /**
+     * Bind your custom express routes here:
+     * Read more: https://expressjs.com/en/starter/basic-routing.html
+     */
+
+    app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*"); // 모든 도메인에서 접근을 허용하도록 설정 (보안상 문제가 있을 수 있으므로 실제 운영 환경에서는 조심히 설정)
+      //   res.header(
+      //     "Access-Control-Allow-Headers",
+      //     "Origin, X-Requested-With, Content-Type, Accept"
+      //   );
+      next();
+    });
+
+    app.get("/hello_world", (req, res) => {
+      res.send("It's time to kick ass and chew bubblegum!");
+    });
+
+    /**
+     * Use @colyseus/playground
+     * (It is not recommended to expose this route in a production environment)
+     */
+    if (process.env.NODE_ENV !== "production") {
+      app.use("/", playground);
+    }
+
+    /**
+     * Use @colyseus/monitor
+     * It is recommended to protect this route with a password
+     * Read more: https://docs.colyseus.io/tools/monitor/#restrict-access-to-the-panel-using-a-password
+     */
+    app.use("/colyseus", monitor());
+  },
+
+  beforeListen: () => {
+    /**
+     * Before before gameServer.listen() is called.
+     */
+  },
+});
---
+++ src/rooms/schema/MyRoomState.ts
@@ -0,0 +1,13 @@
+import { Schema, Context, type, MapSchema } from "@colyseus/schema";
+
+export class Player extends Schema {
+  @type("number") x: number;
+  @type("number") y: number;
+  @type("string") moveState: string;
+}
+
+export class MyRoomState extends Schema {
+  @type("string") mySynchronizedProperty: string = "Hello world";
+
+  @type({ map: Player }) players = new MapSchema<Player>();
+}
